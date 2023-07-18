a4367fb68db04bd71f38646f379c14a647307345
Adrian Budimir <Adrian_Budimir@Trimble.com>
2023-07-18 23:16:01 +0200

    omg

--- prep.sh
+++ prep.sh
@@ -2,4 +2,4 @@
 
 # Getting Trimble Scripts
 
-wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/vpnscript.sh -o vpnscript.sh && wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleify-linux-workstation.sh -o trimbleify-linux-workstation.sh
+wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/vpnscript.sh -o /opt/vpnscript.sh && wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleify-linux-workstation.sh -o /opt/trimbleify-linux-workstation.sh
