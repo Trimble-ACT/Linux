1fa611d1d0aa5b4bbffe9c1e2688031e7ccf23c5
AdrianBudimir <adrian_budimir@trimble.com>
2023-07-19 13:22:39 +0200

    rewhrite

--- trimbleme.sh
+++ trimbleme.sh
@@ -1,9 +1,8 @@
-#! /usr/bin/env bash
 #!/bin/bash
 
 # Getting Trimble Scripts
 
-wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/vpnscript.sh -o /opt/vpnscript.sh && wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleify-linux-workstation.sh -o /opt/trimbleify-linux-workstation.sh && wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleprep.sh -o /opt/trimbleprep.sh
+wget -O vpnscript.sh https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/vpnscript.sh && wget -O trimbleify-linux-workstation.sh https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleify-linux-workstation.sh && wget -O trimbleprep.sh https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleprep.sh
 
 sh /opt/trimbleprep.sh
 #sh /opt/vpnscript.sh
