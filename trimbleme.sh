6832658091f6b3a19612414b5fbb84ad0a29e9f7
AdrianBudimir <adrian_budimir@trimble.com>
2023-07-19 09:20:36 +0200

    almost there

--- trimbleme.sh
+++ trimbleme.sh
@@ -3,6 +3,7 @@
 
 # Getting Trimble Scripts
 
-wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/vpnscript.sh -o /opt/vpnscript.sh && wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleify-linux-workstation.sh -o /opt/trimbleify-linux-workstation.sh
+wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/vpnscript.sh -o /opt/vpnscript.sh && wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleify-linux-workstation.sh -o /opt/trimbleify-linux-workstation.sh && wget https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleprep.sh -o /opt/trimbleprep.sh
 
-sh /opt/vpnscript.sh
+sh /opt/trimbleprep.sh
+#sh /opt/vpnscript.sh
--- trimbleprep.sh
+++ trimbleprep.sh
@@ -4,11 +4,6 @@
 
 sudo dpkg-reconfigure keyboard-configuration && sudo dpkg-reconfigure locales && sudo dpkg-reconfigure tzdata
 
-# intro if you are doing it for a desktop you might want to rip out all vpn related things as chances are you will not use them
-
-echo Trimble VPN Certificate script ; echo Make sure up have downloaded the Certificate file from your email to the ~/Downloads folder
-read -s -n 1 -p "If ready, press any key to continue..."
-
 Red=$'\e[1;31m'
 Green=$'\e[1;32m'
 Blue=$'\e[1;34m'
@@ -33,21 +28,6 @@ echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codenam
 
 sudo apt install firefox
 
-# make directories for Cisco AnyConnect
-
-mkdir -p ~/.cisco/certificates/ca/ && mkdir -p ~/.cisco/certificates/client && mkdir -p ~/.cisco/certificates/client/private
-
-# extract .pem from .pfx
-
-openssl pkcs12 -legacy -in ~/Downloads/*.pfx -nocerts -out ~/.cisco/certificates/ca/CAs.pem -nodes && openssl pkcs12 -legacy -in ~/Downloads/*.pfx -clcerts -nokeys -out ~/.cisco/certificates/client/CL.pem -nodes && openssl rsa -in ~/.cisco/certificates/ca/CAs.pem -out ~/.cisco/certificates/client/private/CL.key && openssl pkcs12 -legacy -in ~/Downloads/*.pfx -cacerts -nokeys -chain -out ~/.cisco/certificates/ca/CA.pem
-
-echo Thanks!
-
-# Install FortiNet client
-
-sudo dpkg -i /opt/forticlient_7.0.7.0246_amd64.deb
-sudo apt install -f
-
 # Install Intune
 # Install dependencies
 sudo apt update
@@ -68,25 +48,6 @@ sudo apt update
 
 # Install Intune management extension agent
 sudo apt install -y mdatp intune-portal microsoft-edge-stable
-
-# FortiCliet old way with issues
-
-#wget -O - https://repo.fortinet.com/repo/7.0/ubuntu/DEB-GPG-KEY | sudo tee /etc/apt/trusted.gpg.d/fortinet.asc
-#echo "deb [arch=amd64] https://repo.fortinet.com/repo/7.0/ubuntu/ /bionic multiverse" | sudo tee -a /etc/apt/sources.list
-#sudo apt-get update && sudo apt install forticlient
-
-# FortiCliet new way wuth other issues
-
-#wget -O - https://repo.fortinet.com/repo/forticlient/7.2/debian/DEB-GPG-KEY | gpg --dearmor | sudo tee /usr/share/keyrings/repo.fortinet.com.gpg
-#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/repo.fortinet.com.gpg] https://repo.fortinet.com/repo/forticlient/7.2/debian/ stable non-free" | sudo tee /etc/apt/sources.list.d/repo.fortinet.com.list
-#sudo apt-get update
-#sudo apt install forticlient
-
-# configure forticlient
-
-#/opt/forticlient/epctrl -r usdayt-ems01.trimble.com -p 8013
-
-# Trimblefy prep
 
 Sudo apt update && sudo apt upgrade -y
 sudo mv /opt/trimbleify-linux-workstation.sh /tmp/trimbleify-linux-workstation.sh
