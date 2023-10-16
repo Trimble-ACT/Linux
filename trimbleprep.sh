9521dcdfddd547d745e67b9d9602a54559cbf7e5
Adrian Budimir <116813823+AdrianBudimir@users.noreply.github.com>
2023-10-16 22:39:35 +0200

    Update trimbleprep.sh

--- trimbleprep.sh
+++ trimbleprep.sh
@@ -4,29 +4,29 @@
 
 sudo dpkg-reconfigure keyboard-configuration && sudo dpkg-reconfigure locales && sudo dpkg-reconfigure tzdata
 
-#Red=$'\e[1;31m'
-#Green=$'\e[1;32m'
-#Blue=$'\e[1;34m'
+Red=$'\e[1;31m'
+Green=$'\e[1;32m'
+Blue=$'\e[1;34m'
 
-#echo $'\e[1;34m' " Closeing FireFox if you have it opened ! "$'\e[0m'
+echo $'\e[1;34m' " Closeing FireFox if you have it opened ! "$'\e[0m'
 
-#sudo pkill -f firefox
+sudo pkill -f firefox
 sudo apt-get purge transmission-gtk
 
 # remove snap & change prio
 
-#echo
-#sudo snap remove firefox && sudo add-apt-repository ppa:mozillateam/ppa
-#echo '
-#Package: *
-#Pin: release o=LP-PPA-mozillateam
-#Pin-Priority: 1001
-#' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-#echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+echo
+sudo snap remove firefox && sudo add-apt-repository ppa:mozillateam/ppa
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
 # install deb FFox
 
-#sudo apt install firefox
+sudo apt install firefox
 
 # Install Intune
 # Install dependencies
