a4fe84fe660b8c7022871d15de46075d2745abec
Adrian Budimir <116813823+AdrianBudimir@users.noreply.github.com>
2023-10-16 14:33:11 +0200

    Update trimbleme.sh

--- trimbleme.sh
+++ trimbleme.sh
@@ -2,7 +2,7 @@
 
 # Getting Trimble Scripts
 
-wget -O vpnscript.sh https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/vpnscript.sh && wget -O trimbleify-linux-workstation.sh https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleify-linux-workstation.sh && wget -O trimbleprep.sh https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleprep.sh
+wget -O trimbleify-linux-workstation.sh https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleify-linux-workstation.sh && wget -O trimbleprep.sh https://raw.githubusercontent.com/Trimble-Technologies-Team/Linux/main/trimbleprep.sh
 
 sh /opt/trimbleprep.sh
 #sh /opt/vpnscript.sh
