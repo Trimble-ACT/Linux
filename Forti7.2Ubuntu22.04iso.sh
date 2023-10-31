d1ecb8e440df75d082838fd083d25643f044bbfa
Adrian Budimir <116813823+AdrianBudimir@users.noreply.github.com>
2023-10-31 13:00:55 +0100

    Update Forti7.2Ubuntu22.04iso.sh

--- Forti7.2Ubuntu22.04iso.sh
+++ Forti7.2Ubuntu22.04iso.sh
@@ -2,7 +2,7 @@
 
 # FortiCliet new way with other issues
 
-wget -O - https://repo.fortinet.com/repo/forticlient/7.2/debian/DEB-GPG-KEY | gpg --dearmor | sudo tee /usr/share/keyrings/repo.fortinet.com.gpg
+wget -O - https://repo.fortinet.com/repo/forticlient/7.2/ubuntu/DEB-GPG-KEY | gpg --dearmor | sudo tee /usr/share/keyrings/repo.fortinet.com.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/repo.fortinet.com.gpg] deb https://repo.fortinet.com/repo/forticlient/7.2/ubuntu/ /stable multiverse" | sudo tee /etc/apt/sources.list.d/repo.fortinet.com.list
 sudo apt-get update
 sudo apt install forticlient
