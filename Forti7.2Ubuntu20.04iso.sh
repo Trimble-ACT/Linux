99bc99e23288fd43d930702f335145ac74ca1f5d
Adrian Budimir <116813823+AdrianBudimir@users.noreply.github.com>
2023-11-07 12:58:22 +0100

    Update Forti7.2Ubuntu20.04iso.sh

--- Forti7.2Ubuntu20.04iso.sh
+++ Forti7.2Ubuntu20.04iso.sh
@@ -3,7 +3,7 @@
 # FortiCliet new way with other issues
 
 wget -O - https://repo.fortinet.com/repo/forticlient/7.2/ubuntu/DEB-GPG-KEY | sudo apt-key add - | gpg --dearmor | sudo tee /usr/share/keyrings/repo.fortinet.com.gpg
-echo "deb [arch=amd64] https://repo.fortinet.com/repo/forticlient/7.2/ubuntu/ /stable multiverse" | sudo tee /etc/apt/sources.list
+echo "deb [arch=amd64] https://repo.fortinet.com/repo/forticlient/7.2/ubuntu/ /stable multiverse" >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt install forticlient
 
